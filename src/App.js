1c54132007d78e73ac2d1e4a5962f2a76e2bfc4e
Wenhao Lee <wenceslauslee@gmail.com>
2020-05-22 14:57:17 -0400

    Add basic tables

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import DataTable from "./components/DataTable";
 import React from 'react';
 import Tab from 'react-bootstrap/Tab'
 import TabContent from 'react-bootstrap/TabContent'
@@ -16,12 +17,17 @@ function App() {
         <p className="tab"><br/><br/><br/>-<a href="https://www.linkedin.com/in/wenceslauslee/">Wences Lee</a></p>
       </header>
       <header className="App-body">
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tabs defaultActiveKey="countyranking" id="tab-table">
           <Tab eventKey="countyranking" title="County Ranking">
-            <TabContent />
+            <TabContent>
+              <DataTable/>
+            </TabContent>
           </Tab>
           <Tab eventKey="stateranking" title="State Ranking">
-            <TabContent />
+            <TabContent>Goodbye</TabContent>
+          </Tab>
+          <Tab eventKey="statecountysearch" title="Search By">
+            <TabContent>Goodbye</TabContent>
           </Tab>
         </Tabs>
       </header>
