dd58e470a459c958ea49a3bea5a419e2d6057211
Wenhao Lee <wenceslauslee@gmail.com>
2020-06-15 23:43:27 -0400

    Add increase case count

--- src/components/SearchByState.js
+++ src/components/SearchByState.js
@@ -49,10 +49,14 @@ class SearchByState extends Component {
       caseCountDataPoints: [],
       caseCountMax: 0,
       caseCountTracker: null,
+      caseCountIncreaseDataPoints: [],
+      caseCountIncreaseMax: 0,
+      caseCountIncreaseTracker: null,
     };
 
     this.submitPlot = this.submitPlot.bind(this);
     this.handleTrackerChanged1 = this.handleTrackerChanged1.bind(this);
+    this.handleTrackerChanged2 = this.handleTrackerChanged2.bind(this);
   }
 
   componentDidMount() {
@@ -99,7 +103,9 @@ class SearchByState extends Component {
       showTables: true,
       columns: stateColumns,
       caseCountDataPoints: results.caseCount,
-      caseCountMax: results.caseCountMax
+      caseCountMax: results.caseCountMax,
+      caseCountIncreaseDataPoints: results.caseCountIncrease,
+      caseCountIncreaseMax: results.caseCountIncreaseMax
     }));
   }
 
@@ -126,20 +132,27 @@ class SearchByState extends Component {
     const results = {};
     const caseCount = [];
     var caseCountMax = 0;
+    const caseCountIncrease = [];
+    var caseCountIncreaseMax = 0;
 
     for (var index in allStates[0]) {
       caseCount.push([allStates[0][index][0]]);
+      caseCountIncrease.push([allStates[0][index][0]]);
     }
 
     for (var i = 0; i < allStates.length; i++) {
       for (var j = 0; j < allStates[i].length; j++) {
         caseCountMax = Math.max(caseCountMax, allStates[i][j][1]);
         caseCount[j].push(allStates[i][j][1]);
+        caseCountIncreaseMax = Math.max(caseCountIncreaseMax, allStates[i][j][3]);
+        caseCountIncrease[j].push(allStates[i][j][3]);
       }
     }
 
     results.caseCount = caseCount;
     results.caseCountMax = Math.round(caseCountMax * 1.05);
+    results.caseCountIncrease = caseCountIncrease;
+    results.caseCountIncreaseMax = Math.round(caseCountIncreaseMax * 1.05);
     return results;
   }
 
@@ -149,24 +162,25 @@ class SearchByState extends Component {
     return colors[order];
   }
 
-  getCaseCountGraph() {
+  getGraph(seriesName, titleName, seriesDataPoints, tracker, handleTrackerChanged, dataMax) {
     if (this.state.showTables && !this.state.loading) {
       const lowerCaseColumns = this.state.columns.map(state => state.toLowerCase());
       const series = new TimeSeries(
         {
-          name: "CaseCount",
+          name: seriesName,
           columns: lowerCaseColumns,
-          points: this.state.caseCountDataPoints
+          points: seriesDataPoints
         }
       );
 
       let dateValue;
       const stateLegendValues = [];
-      if (this.state.caseCountTracker) {
-        const index = series.bisect(this.state.caseCountTracker);
+      if (tracker) {
+        const index = series.bisect(tracker);
         const trackerEvent = series.at(index);
         const utcDate = trackerEvent.timestamp();
-        dateValue = `${utcDate.getFullYear()}-${('0' + (utcDate.getMonth() + 1)).slice(-2)}-${('0' + utcDate.getDate()).slice(-2)}`;
+        dateValue = `${utcDate.getFullYear()}-${('0' + (utcDate.getMonth() + 1)).slice(-2)}-${('0' +
+          utcDate.getDate()).slice(-2)}`;
 
         for (var i = 1; i < lowerCaseColumns.length; i++) {
           stateLegendValues.push(`${trackerEvent.get(lowerCaseColumns[i])}`);
@@ -205,13 +219,13 @@ class SearchByState extends Component {
 
       return (
         <div>
-          <ChartContainer title='Case Counts' timeRange={ series.range() }
-            width={ 400 } showGrid={ true } titleStyle={{ fill: '#000000', fontWeight: 500 }} timeAxisStyle={ darkAxis }
+          <ChartContainer title={ titleName } timeRange={ series.range() }
+            width={ 600 } showGrid={ true } titleStyle={{ fill: '#000000', fontWeight: 500 }} timeAxisStyle={ darkAxis }
             minTime={ series.range().begin() } maxTime={ series.range().end() } timeAxisTickCount={ 10 }
-            onTrackerChanged={ this.handleTrackerChanged1 }>
+            onTrackerChanged={ handleTrackerChanged }>
             <TimeAxis format="day"/>
             <ChartRow height='400'>
-              <YAxis id="y" label="Count" min={ 0 } max={ this.state.caseCountMax } width="60" type="linear" showGrid
+              <YAxis id="y" label="Count" min={ 0 } max={ dataMax } width="60" type="linear" showGrid
                 style={ darkAxis } />
                <Charts>
                 <LineChart axis="y" series={ series } columns={ yColumns } style={ style }
@@ -229,10 +243,29 @@ class SearchByState extends Component {
     return null;
   }
 
+  getCaseCountGraph() {
+    return this.getGraph(
+      'CaseCount', 'Case Count', this.state.caseCountDataPoints, this.state.caseCountTracker,
+      this.handleTrackerChanged1, this.state.caseCountMax);
+  }
+
+  getCaseCountIncreaseGraph() {
+    return this.getGraph(
+      'CaseCountIncrease', 'Case Count Increase', this.state.caseCountIncreaseDataPoints,
+      this.state.caseCountIncreaseTracker, this.handleTrackerChanged2, this.state.caseCountIncreaseMax);
+  }
+
   handleTrackerChanged1(tracker) {
     this.setState(prevState => ({
       ...prevState,
       caseCountTracker: tracker,
+    }));
+  }
+
+  handleTrackerChanged2(tracker) {
+    this.setState(prevState => ({
+      ...prevState,
+      caseCountIncreaseTracker: tracker,
     }));
   }
 
@@ -261,11 +294,22 @@ class SearchByState extends Component {
           </form>
           <Button variant="warning" style={{ 'margin-left': '10px' }} onClick={ this.submitPlot }>Plot!</Button>
         </div>
+        { this.state.loading ?
+            <div style={{ display: 'inline-block', textAlign: 'center', minWidth: '1000px', marginTop: '80px' }}>
+              <Spinner animation="border" />
+            </div> :
+            ''
+        }
         <div style={{ display: 'flex', minWidth: '1200px' }}>
           <div style={{ 'marginTop': '30px', 'marginBottom': '10px' }}>
             { this.getCaseCountGraph() }
           </div>
           <div style={{ marginLeft: '30px', marginTop: '30px', marginBottom: '10px' }}>
+            { this.getCaseCountIncreaseGraph() }
+          </div>
+        </div>
+        <div style={{ display: 'flex', minWidth: '1200px' }}>
+          <div style={{ 'marginTop': '30px', 'marginBottom': '10px' }}>
             { this.getCaseCountGraph() }
           </div>
           <div style={{ marginLeft: '30px', marginTop: '30px', marginBottom: '10px' }}>
